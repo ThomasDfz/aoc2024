ff50fa7486943fca118cf7b46c54a8e9c7813772
Thomas Défossez <thomas.defossez-isc.france@ibm.com>
2024-12-07 11:05:50 +0100

    Day 7

---
+++ 7/index.js
@@ -0,0 +1,40 @@
+const { input, example } = require('../utils/parser');
+
+const DEBUG = false;
+
+const equations = (DEBUG ? example : input)
+  .split('\n')
+  .map(line => {
+    const [target, operands] = line.split(': ');
+
+    return { target: Number(target), operands: operands.split(' ').map(Number) };
+  });
+
+const add = (operand, remainings) => [operand + remainings[0], ...remainings.slice(1)];
+const multiply = (operand, remainings) => [operand * remainings[0], ...remainings.slice(1)];
+const concat = (operand, remainings) => [Number([operand, remainings[0]].join('')), ...remainings.slice(1)];
+
+const isValid = (target, operands, operators) => {
+  if (operands[0] > target) {
+    return false;
+  }
+
+  if (operands.length === 1) {
+    return operands[0] === target;
+  }
+
+  const [operand, ...remainings] = operands;
+
+  return operators.some(operator => isValid(target, operator(operand, remainings), operators));
+};
+
+const part1 = () => {
+  return equations.reduce((acc, { target, operands }) => acc + (isValid(target, operands, [add, multiply]) ? target : 0), 0);
+};
+
+const part2 = () => {
+  return equations.reduce((acc, { target, operands }) => acc + (isValid(target, operands, [add, multiply, concat]) ? target : 0), 0);
+};
+
+console.log(`Part 1 : ${part1()}`);
+console.log(`Part 2 : ${part2()}`);
